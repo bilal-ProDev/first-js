29695ed51c8851b6845ecde703e773ac5dfe86fd
bilal-ProDev <155066386+bilal-ProDev@users.noreply.github.com>
2024-01-01 17:28:37 +0000

    commet code

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,14 +13,42 @@ console.log(valueInNumber);
 
 
 let isLoggedIn = 1
+
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
 
 // 1 => ture; 0=> false;
-// "" => false
+// "" => falsehtuture 
 // "hitesh" => true
 
 let someNumber =33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 
+const newUser = new Object();
+
+const course ={
+    coursename = "js in urdu",
+    price: "999",
+    courseTeacher: "ahmad"
+}
+
+// course.courseInstructor
+
+// console.log(courseInstructor);
+
+const {courseInstructor: instructor} = course;
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("please enter a username");
+        return;
+    }
+
+    return  `${username} just logged in`;
+}
+console.log(loginUserMessage()); // undefined just logged in
+function calculateCartPrice(...num1){
+    return num1;
+}
+console.log(calculateCartPrice(200, 400, 500, 2000));
